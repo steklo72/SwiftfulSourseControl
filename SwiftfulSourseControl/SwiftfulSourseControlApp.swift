9d41e67749062e2db225be064c7d1d196ca7d454
Fedotov <sasha4936@yandex.ru>
2024-05-27 14:31:10 +0500

    Initial Commit

---
+++ SwiftfulSourseControl/SwiftfulSourseControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourseControlApp.swift
+//  SwiftfulSourseControl
+//
+//  Created by Fedotov Aleksandr on 27.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourseControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
