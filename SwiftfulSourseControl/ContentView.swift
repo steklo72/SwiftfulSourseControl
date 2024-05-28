6ba1fc886533acccbf13630df03a32e11b21de4f
Fedotov <sasha4936@yandex.ru>
2024-05-28 23:39:12 +0500

    [feature] Subscribe me now

--- SwiftfulSourseControl/ContentView.swift
+++ SwiftfulSourseControl/ContentView.swift
@@ -15,12 +15,10 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("SwiftFulThinging!")
-            Button("Click me") {
+            Button("Subscribe me") {
                 
             }
-            Button("Click me") {
-                
-            }
+            
         }
         .padding()
         
