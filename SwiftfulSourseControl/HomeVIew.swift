4269c693b9527eeee404f537dcf702ebd6ee88d3
Fedotov <sasha4936@yandex.ru>
2024-05-28 09:25:25 +0500

    Add HomeView

---
+++ SwiftfulSourseControl/HomeVIew.swift
@@ -0,0 +1,19 @@
+//
+//  HomeVIew.swift
+//  SwiftfulSourseControl
+//
+//  Created by Fedotov Aleksandr on 28.05.2024.
+//
+
+import SwiftUI
+
+struct HomeVIew: View {
+    var body: some View {
+        Text("Screen2")
+        Text("Screen2")
+    }
+}
+
+#Preview {
+    HomeVIew()
+}
